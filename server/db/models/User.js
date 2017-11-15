b761a27483fc800b9f83190b3ddfc7610ef8066e
Francis Ngo <francis.t.ngo@gmail.com>
2017-11-15 10:58:14 -0800

    Change model fields from snake case to camel case

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -2,24 +2,19 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     username: {
       type: DataTypes.STRING,
-      allowNull: false,
       unique: true,
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: false,
     },
-    first_name: {
+    firstName: {
       type: DataTypes.STRING,
-      allowNull: true,
     },
-    last_name: {
+    lastName: {
       type: DataTypes.STRING,
-      allowNull: true,
     },
     email: {
       type: DataTypes.STRING,
-      allowNull: false,
       unique: true,
       validate: {
         isEmail: true,
