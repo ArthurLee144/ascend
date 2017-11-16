534153d581b14cdc97706cac8ed2baa72ac0f1a6
Francis Ngo <francis.t.ngo@gmail.com>
2017-11-15 22:02:14 -0800

    Create components directory

--- client/routes/Main.jsx
+++ client/routes/Main.jsx
@@ -1,16 +1,5 @@
 import React from 'react';
-import { graphql } from 'react-apollo';
-import gql from 'graphql-tag';
 
-const usersQuery = gql`
-  {
-    users {
-      id
-      username
-    }
-  }
-`;
+const Main = () => <h1>This is the Main page!</h1>;
 
-const Main = ({ data: { users = [] } }) => users.map(user => <h1 key={user.id}>{user.username}</h1>);
-
-export default graphql(usersQuery)(Main);
+export default Main;
--- client/routes/index.js
+++ client/routes/index.js
@@ -5,26 +5,24 @@ import {
   Switch,
 } from 'react-router-dom';
 
-import Landing from './Landing';
-import Login from './Login';
 import Main from './Main';
+import Login from './Login';
+import Search from './Search';
+import Details from './Details';
+import Review from './Review';
 import Create from './Create';
 import Profile from './Profile';
-import Details from './Details';
-import Search from './Search';
-import Review from './Review';
 
 export default () => (
   <Router>
     <Switch>
       <Route exact path="/" component={Main} />
+      <Route path="/login" component={Login} />
+      <Route path="/search" component={Search} />
+      <Route path="/details" component={Details} />
+      <Route path="/writeareview" component={Review} />
       <Route path="/createasite" component={Create} />
       <Route path="/profile" component={Profile} />
-      <Route path="/landing" component={Landing} />
-      <Route path="/login" component={Login} />
-      <Route path="/details" component={Details} />
-      <Route path="/search" component={Search} />
-      <Route path="/writeareview" component={Review} />
       <Route render={() => <p>Not Found</p>} />
     </Switch>
   </Router>
