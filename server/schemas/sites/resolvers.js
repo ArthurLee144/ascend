606920d090a70184dfcf73f94fda3e3e6454fdf5
Francis Ngo <francis.t.ngo@gmail.com>
2017-11-15 16:13:34 -0800

    Fix site query and mutation resolvers

--- server/schemas/sites/resolvers.js
+++ server/schemas/sites/resolvers.js
@@ -1,37 +1,19 @@
 const models = require('../../db/models');
 
-module.exports = async function getSiteByID(parentValue, { id }) {
-  await models.Site.findOne({ where: { id } });
-};
+module.exports.getSiteByID = (parentValue, { id }) => models.Site.findOne({ where: { id } });
 
-module.exports = async function getAllSites() {
-  await models.Site.findAll();
-};
+module.exports.getAllSites = () => models.Site.findAll();
 
-module.exports = async function createSite(parentValue, {
+module.exports.createSite = (parentValue, {
   name, address, city, state, zipCode,
-}) {
-  await models.Site.create({
-    name,
-    address,
-    city,
-    state,
-    zipCode,
-  });
-};
-
-module.exports = async function updateSite(parentValue, {
+}) => models.Site.create({
   name, address, city, state, zipCode,
-}) {
-  await models.Site.create({
-    name,
-    address,
-    city,
-    state,
-    zipCode,
-  });
-};
+});
 
-module.exports = async function removeSite(parentValue, { id }) {
-  await models.Site.destroy({ where: { id } });
-};
+module.exports.updateSite = (parentValue, {
+  name, address, city, state, zipCode,
+}) => models.Site.create({
+  name, address, city, state, zipCode,
+});
+
+module.exports.removeSite = (parentValue, { id }) => models.Site.destroy({ where: { id } });
