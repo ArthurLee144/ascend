8e55b970fa70e630ec77edf7cf700af27ae636ad
Francis Ngo <francis.t.ngo@gmail.com>
2017-11-15 16:31:57 -0800

    Rename createHttpLink to HttpLink

--- client/index.js
+++ client/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
+import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
 import Routes from './routes';
 
 const client = new ApolloClient({
-  link: createHttpLink({ uri: 'http://localhost:8080/graphql' }),
+  link: new HttpLink({ uri: 'http://localhost:8080/graphql' }),
   cache: new InMemoryCache(),
 });
 
@@ -17,7 +17,4 @@ const App = (
   </ApolloProvider>
 );
 
-ReactDOM.render(
-  App,
-  document.getElementById('root'),
-);
+ReactDOM.render(App, document.getElementById('root'));
