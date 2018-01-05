2aa3c2332160f9811d7871de245003c58cff467a
Francis Ngo <francis.t.ngo@gmail.com>
2018-01-04 22:46:50 -0800

    Add basic Bulma components to Main page

--- client/routes/Main.jsx
+++ client/routes/Main.jsx
@@ -1,18 +1,114 @@
-import React from 'react';
-import { graphql } from 'react-apollo';
-import gql from 'graphql-tag';
+import React, { Component } from 'react';
+// import { graphql } from 'react-apollo';
+// import gql from 'graphql-tag';
+//
+// const allUsersQuery = gql`
+//   {
+//     getAllUsers {
+//       id
+//       email
+//     }
+//   }
+// `;
+//
+// const Main = ({ data: { getAllUsers = [] } }) =>
+//   getAllUsers.map(user =>
+//     <h1 key={user.id}>{user.email}</h1>);
+//
+// export default graphql(allUsersQuery)(Main);
 
-const allUsersQuery = gql`
-  {
-    getAllUsers {
-      id
-      email
-    }
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
-`;
 
-const Main = ({ data: { getAllUsers = [] } }) =>
-  getAllUsers.map(user =>
-    <h1 key={user.id}>{user.email}</h1>);
+  render() {
+    return (
+      <div>
+        <section className="hero is-info is-medium is-bold">
+          <div className="hero-body">
+            <div className="container has-text-centered">
+              <h1 className="title">
+                Ascend
+              </h1>
+              <h2 className="subtitle">
+                Your next outdoor bouldering site awaits.
+              </h2>
+            </div>
+          </div>
+        </section>
 
-export default graphql(allUsersQuery)(Main);
+        <div className="box cta">
+          <p className="has-text-centered">
+            <span className="tag is-primary">New</span> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+        </div>
+
+        <section className="container">
+          <div className="columns features">
+            <div className="column is-3">
+              <div className="card">
+                <div className="card-image has-text-centered">
+                  <i className="fa fa-camera" />
+                </div>
+                <div className="card-content">
+                  <div className="content">
+                    <h4>Tristique senectus et netus et. </h4>
+                    <p>Purus semper eget duis at tellus at urna condimentum mattis. Non blandit massa enim nec. Integer enim neque volutpat ac tincidunt vitae semper quis. Accumsan tortor posuere ac ut consequat semper viverra nam.</p>
+                    <p><a href="#">Learn more</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="column is-3">
+              <div className="card">
+                <div className="card-image has-text-centered">
+                  <i className="fa fa-camera" />
+                </div>
+                <div className="card-content">
+                  <div className="content">
+                    <h4>Tristique senectus et netus et. </h4>
+                    <p>Purus semper eget duis at tellus at urna condimentum mattis. Non blandit massa enim nec. Integer enim neque volutpat ac tincidunt vitae semper quis. Accumsan tortor posuere ac ut consequat semper viverra nam.</p>
+                    <p><a href="#">Learn more</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="column is-3">
+              <div className="card">
+                <div className="card-image has-text-centered">
+                  <i className="fa fa-camera" />
+                </div>
+                <div className="card-content">
+                  <div className="content">
+                    <h4>Tristique senectus et netus et. </h4>
+                    <p>Purus semper eget duis at tellus at urna condimentum mattis. Non blandit massa enim nec. Integer enim neque volutpat ac tincidunt vitae semper quis. Accumsan tortor posuere ac ut consequat semper viverra nam.</p>
+                    <p><a href="#">Learn more</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="column is-3">
+              <div className="card">
+                <div className="card-image has-text-centered">
+                  <i className="fa fa-camera" />
+                </div>
+                <div className="card-content">
+                  <div className="content">
+                    <h4>Tristique senectus et netus et. </h4>
+                    <p>Purus semper eget duis at tellus at urna condimentum mattis. Non blandit massa enim nec. Integer enim neque volutpat ac tincidunt vitae semper quis. Accumsan tortor posuere ac ut consequat semper viverra nam.</p>
+                    <p><a href="#">Learn more</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
